f0dd971d391bd4e6c7ccec4beb59408d9db94d42
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2020-11-05 17:31:06 +0100

    add some global changes for new look

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,7 @@ fetch("https://api.github.com/users/kamilMalinowski/repos")
     for (let repo of resp) {
       const repositoryList = document.querySelector(".list--js");
       const { name, html_url } = repo;
-      const myTemplate = 
-      `<li class="list__element">${name} <a class="link" href="${html_url}" title="link do repozytorium ${name} na githubie">link do githuba</a></li>`;
+      const myTemplate = `<li class="list__element">${name} <a class="link" href="${html_url}" title="link do repozytorium ${name} na githubie">link do githuba</a></li>`;
       repositoryList.innerHTML += myTemplate;
     }
   })
@@ -34,40 +33,34 @@ const switchModes = document.querySelector(".darkmode");
 
 switchModes.addEventListener("click", () => {
   if (isDark) {
-    document.documentElement.style.setProperty(
-      "--bacground1",
-      "rgb(250, 255, 238)"
-    );
-    document.documentElement.style.setProperty("--color-black", "black");
-    document.documentElement.style.setProperty("--color-termina", "#686a6b");
-    document.documentElement.style.setProperty("--color-js", "#f7df1e");
-    document.documentElement.style.setProperty("--color-css", "#58a3e6");
-    document.documentElement.style.setProperty("--color-html", "#ff761e");
-    document.documentElement.style.setProperty("--link-color", "#ff75bb");
+    document.documentElement.style.setProperty("--color-bg", "#faffee");
+    document.documentElement.style.setProperty("--color-lightGreen", "#daff82");
+    document.documentElement.style.setProperty("--color-purple", "#a38fffa6b");
+    document.documentElement.style.setProperty("--color-orange", "#ffd475");
+    document.documentElement.style.setProperty("--color-pink", "#e86bab");
+    document.documentElement.style.setProperty("--color-green", "#75ffc2");
+
     document.documentElement.style.setProperty("--color-gray", "#333333");
-    document.documentElement.style.setProperty("--color-darkpurple", "#796db3");
-    document.documentElement.style.setProperty("--color-purple", "#a38fff");
-    document.documentElement.style.setProperty("--color-green", "#daff82");
-    document.documentElement.style.setProperty("--dot", "white");
-    document.documentElement.style.setProperty("--click-menu-button", "white");
+    // document.documentElement.style.setProperty("--color-darkpurple", "#796db3");
+    // document.documentElement.style.setProperty("--color-purple", "#a38fff");
+    // document.documentElement.style.setProperty("--color-green", "#daff82");
+    // document.documentElement.style.setProperty("--dot", "white");
+    // document.documentElement.style.setProperty("--click-menu-button", "white");
     isDark = false;
   } else {
-    document.documentElement.style.setProperty(
-      "--bacground1",
-      "rgb(17, 17, 17)"
-    );
-    document.documentElement.style.setProperty("--color-black", "white");
-    document.documentElement.style.setProperty("--color-termina", "#000000");
-    document.documentElement.style.setProperty("--color-js", "#443d07");
-    document.documentElement.style.setProperty("--color-css", "#26425c");
-    document.documentElement.style.setProperty("--color-html", "#442009");
-    document.documentElement.style.setProperty("--link-color", "#834664");
-    document.documentElement.style.setProperty("--color-gray", "#d3cbcb");
-    document.documentElement.style.setProperty("--color-darkpurple", "#29253a");
-    document.documentElement.style.setProperty("--color-purple", "#6e6591");
-    document.documentElement.style.setProperty("--color-green", "#2b331a");
-    document.documentElement.style.setProperty("--dot", "black");
-    document.documentElement.style.setProperty("--click-menu-button", "grey");
+    document.documentElement.style.setProperty("--color-bg", "#333333");
+    document.documentElement.style.setProperty("--color-lightGreen", "#333333");
+    document.documentElement.style.setProperty("--color-purple", "#333333");
+    document.documentElement.style.setProperty("--color-orange", "#333333");
+    document.documentElement.style.setProperty("--color-pink", "#333333");
+    document.documentElement.style.setProperty("--color-green", "#333333");
+
+    document.documentElement.style.setProperty("--color-gray", "#faffee");
+    // document.documentElement.style.setProperty("--color-darkpurple", "#29253a");
+    // document.documentElement.style.setProperty("--color-purple", "#6e6591");
+    // document.documentElement.style.setProperty("--color-green", "#2b331a");
+    // document.documentElement.style.setProperty("--dot", "black");
+    // document.documentElement.style.setProperty("--click-menu-button", "grey");
     isDark = true;
   }
 });
@@ -75,5 +68,3 @@ switchModes.addEventListener("click", () => {
 // const time = moment().endOf("week").fromNow();
 // const timePlaceholder = document.querySelector(".time--js");
 // timePlaceholder.innerHTML = time;
-
-
